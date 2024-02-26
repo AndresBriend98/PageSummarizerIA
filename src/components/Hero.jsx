8c13ccd7fd3a2dde1aa741e836d0bbbe91a983e8
Andres Briend <briendandres@gmail.com>
2024-02-26 18:36:08 -0300

    finish now

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,8 @@ import { logo } from "../assets";
 const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
-      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='sumz_logo' className='w-20 object-contain' />
+      <nav className='flex justify-start items-center w-full mb-10 pt-3'>
+        <img src={logo} alt='logo' className='w-20 object-contain' />
       </nav>
 
       <h1 className='head_text'>
