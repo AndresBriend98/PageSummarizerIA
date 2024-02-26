0056e7adf856734be9c9989b998c442760270230
Andres Briend <briendandres@gmail.com>
2024-02-26 18:26:30 -0300

    finish finish

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,16 +7,6 @@ const Hero = () => {
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
         <img src={logo} alt='sumz_logo' className='w-20 object-contain' />
-
-        <button
-          type='button'
-          onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
-          }
-          className='black_btn'
-        >
-          GitHub
-        </button>
       </nav>
 
       <h1 className='head_text'>
